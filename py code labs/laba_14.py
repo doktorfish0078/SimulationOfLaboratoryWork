661714f7cb0a6f162ab473f4158405d49ecec94f
Flint01686 <44868229+Flint01686@users.noreply.github.com>
2020-10-29 23:20:19 +0400

    lb14_prefinal
    Some fix
    New shtok move system

--- py code labs/laba_14.py
+++ py code labs/laba_14.py
@@ -9,18 +9,24 @@ from converted_forms_to_py import laba14
 
 
 class Laba14(QMainWindow, laba14.Ui_Laba14):
+    # global const
     amperage = 0.0
+    stok_x = 0
+    stok_y = 0
+
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
+        # const ini
+        self.stok_x = self.label_stock.geometry().getCoords()[0]
+        self.stok_y = self.label_stock.geometry().getCoords()[1]
         # set pixmaps
         self.label_instrumentation.setPixmap(QPixmap("..\\images\\laba_14\\pribori.png"))
         self.label_stock.setPixmap(QPixmap("..\\images\\laba_14\\stock.png"))
         self.label_solenoid.setPixmap(QPixmap("..\\images\\laba_14\\solenoid.png"))
 
-        #connects
+        # connects
         self.amperDial.valueChanged.connect(self.set_amperage)
         self.amperDial.valueChanged.connect(self.set_induction)
         self.slider_stock.valueChanged.connect(self.set_induction)
@@ -32,19 +38,24 @@ class Laba14(QMainWindow, laba14.Ui_Laba14):
 
     def set_induction(self):
         # constants
-        sl = 10 ** (-2) * self.slider_stock.value()
-        self.shtokValue.move(480 + self.slider_stock.value() * 4.2, 460)
-        self.shtokValue.setText(str(47 - self.slider_stock.value()))
+        shtk_dist = (float(self.slider_stock.value())/2.0);
+        sl = 10 ** (-2) * shtk_dist
+        self.shtokValue.setText(' ' + str(47 - shtk_dist))
 
-        self.shtokRealValue.move(480 + self.slider_stock.value() * 4.2, 580)
-        self.shtokRealValue.setText(str(self.slider_stock.value()))
-
+        print(shtk_dist)
+        print(self.slider_stock.value()+6)
         self.label_stock.move(
-            90 + self.slider_stock.value() * 9.6,
-            self.label_stock.geometry().getCoords()[1])
+            # self.slider_stock.
+            self.stok_x+(self.slider_stock.value()+6)*5-0.12*(self.slider_stock.value()+6),
+            self.stok_y)
+
+        self.label_stock_head.move(
+            # self.slider_stock.
+            self.label_stock.geometry().getCoords()[2],
+            self.label_stock_head.geometry().getCoords()[1])
 
         if sl < 0:
-            self.mainOut.setText('0.0')
+            self.mainOut.setText(str("%.4f" % 0))
             return
         mu = 10 ** (-7) * 12.57
         n = 2000
@@ -56,7 +67,7 @@ class Laba14(QMainWindow, laba14.Ui_Laba14):
         # print("%.15f" % (mu * self.amperage * n / 2))
         # print("%.15f" % ((len - sl) / sqrt(r ** 2 + (len - sl) ** 2)))
         # print("%.15f" % (sl / sqrt(r ** 2 + sl ** 2)))
-        self.mainOut.setText("%.15f" % (1000 * (mu * self.amperage * n / 2) *
+        self.mainOut.setText("%.4f" % (1000 * (mu * self.amperage * n / 2) *
                                            ((len - sl) / sqrt(r ** 2 + (len - sl) ** 2) + sl / sqrt(r ** 2 + sl ** 2))))
 
 if __name__ == '__main__':
